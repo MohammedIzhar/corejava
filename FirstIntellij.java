e5637c0aa97e8f6c049161a93033bc05112aa462
MohammedIzhar <izharjava@gmail.com>
2017-06-30 09:42:34 +0530

    Add files via upload
    First code written in Intellij IDE

---
+++ FirstIntellij.java
@@ -0,0 +1,10 @@
+package com.first.intellij;
+
+/**
+ * Created by izhar.ahmed on 5/18/2017.
+ */
+public class FirstIntellij {
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+}
